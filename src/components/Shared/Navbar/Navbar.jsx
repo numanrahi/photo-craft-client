d696713459ecba2e6609c0f29dcfe0e5ae7ec0b8
numanrahi <arahi2994@gmail.com>
2023-06-09 14:59:31 +0600

    navbar added

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -3,16 +3,20 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div className='d-flex justify-content-around p-3'>
-            <div className=''>name</div>
-            <div >
-                <Link className='px-3 text-decoration-none'>Home</Link>
-                <Link className='px-3 text-decoration-none'>Instructors</Link>
-                <Link className='px-3 text-decoration-none'>Classes</Link>
-                <Link className='px-3 text-decoration-none'>Dashboard</Link>
+        <div className='d-flex justify-content-around py-3 text-bg-dark align-items-center'>
+            <div className='fs-2'>PhotoCraft Academy</div>
+            <div className='fs-4'>
+                <Link to="/" className='px-3 text-decoration-none text-white'>Home</Link>
+                <Link className='px-3 text-decoration-none text-white'>Instructors</Link>
+                <Link className='px-3 text-decoration-none text-white'>Classes</Link>
+                <Link className='px-3 text-decoration-none text-white'>Dashboard</Link>
             </div>
-            <div>Login</div>
+            <div>
+                <Link to="/login"><button className='px-3 py-2 btn btn-primary rounded-lg'>Login</button></Link>
+            </div>
+
         </div>
+
     );
 };
 
