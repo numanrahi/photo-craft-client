3388b2815c60bfa6c687f67fed1f668735131acc
numanrahi <arahi2994@gmail.com>
2023-06-13 14:39:00 +0600

    instructors add a class page added

--- src/components/pages/InstructorsAddClass/InstructorsAddClass.jsx
+++ src/components/pages/InstructorsAddClass/InstructorsAddClass.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from "react";
+import SingleInstructorClass from "./SingleInstructorClass";
+import { AuthContext } from "../../../providers/AuthProvider";
 
 const InstructorsAddClass = () => {
-    return (
+  const [all_class, setAll_classes] = useState([]);
+  const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/classes")
+      .then((res) => res.json())
+      .then((data) => {
+        const filtered_data = data.filter(
+          (single) => single?.instructor_email === user.email
+        );
+        setAll_classes(filtered_data);
+      });
+  }, []);
+
+  const all_students = all_class.map((single) => single.students);
+  const sum = all_students.reduce((total, num) => total + Number(num), 0);
+
+  console.log('all classes',all_class);
+  return (
+    <div>
+      <div className="p-5">
+        <div className="fs-4 pb-3 ps-3">Total enrolled students : {sum}</div>
         <div>
-            instructor add a class
+          {all_class.map((classData) => (
+            <SingleInstructorClass
+              classData={classData}
+              key={classData._id}
+            ></SingleInstructorClass>
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default InstructorsAddClass;
---
+++ src/components/pages/InstructorsAddClass/SingleInstructorClass.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+// import "./SingleInstructorClass.css";
+
+const SingleInstructorClass = ({ classData }) => {
+	const { name, image, status , students } = classData;
+
+	return (
+		<div>
+			<div className="p-3">
+				<div className="row align-items-center i-card-class">
+					<div>
+						<img
+							className="img-fluid i-img-class"
+							src={image}
+							alt="class"
+						/>
+						<div className="fs-3 pt-2">{name}</div>
+					</div>
+					<div className="d-flex align-items-center justify-content-between">
+						<div>
+							<div>
+								<div>status : {status}</div>
+							</div>
+							<div>
+								<div>Enrolled Students : {students}</div>
+							</div>
+						</div>
+						<div className="btn bg-dark rounded-0 text-white px-5">
+							Update This Class
+						</div>
+					</div>
+					<div className="py-2">
+						<hr/>
+					</div>
+					<div>
+						No Feedback from Admin
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default SingleInstructorClass;
