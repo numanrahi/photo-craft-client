43b039f098fcc63d6ea4467336b297f2a80c3083
numanrahi <arahi2994@gmail.com>
2023-06-13 16:50:47 +0600

    admin manage classes function added

--- src/components/pages/AdminManageClasses/AdminManageClasses.jsx
+++ src/components/pages/AdminManageClasses/AdminManageClasses.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import AdminSingleClass from "./AdminSingleClass";
 
-const AdminMangeClasses = () => {
-    return (
-        <div>
-            <h2>Admin manage classes</h2>
-        </div>
-    );
+const AdminManageClasses = () => {
+	const [all_class, setAll_classes] = useState([]);
+	const [test, setTest] = useState(false);
+
+	useEffect(() => {
+		fetch("http://localhost:5000/classes")
+			.then((res) => res.json())
+			.then((data) => setAll_classes(data));
+	}, [all_class, test]);
+
+	return (
+		<div className="p-4">
+			{all_class.map((single) => (
+				<AdminSingleClass
+					key={single._id}
+					single={single}
+					setTest={setTest}
+				></AdminSingleClass>
+			))}
+		</div>
+	);
 };
 
-export default AdminMangeClasses;
+export default AdminManageClasses;
---
+++ src/components/pages/AdminManageClasses/AdminSingleClass.jsx
@@ -0,0 +1,106 @@
+import React, { useEffect, useState } from "react";
+import Swal from "sweetalert2";
+
+const AdminSingleClass = ({ single, setTest }) => {
+  const [unSelectable, setUnSelectable] = useState(false);
+
+  const {
+    image,
+    name,
+    instructor_email,
+    instructor_name,
+    seats,
+    price,
+    status,
+    _id,
+  } = single;
+
+  const [newStatus, setNewStats] = useState(status);
+
+  const onApprove = () => {
+    Swal.fire(
+      "Thanks for Approving !",
+      "You may need to wait for a couple of seconds for the server to respond and automatically refresh the role . Thanks for your patience .",
+      "success"
+    );
+    setNewStats("approved");
+    setTest(true);
+    return;
+  };
+  const onDeny = () => {
+    Swal.fire(
+      "Class add request denied !",
+      "You may need to wait for a couple of seconds for the server to respond and automatically refresh the role . Thanks for your patience .",
+      "success"
+    );
+    setNewStats("denied");
+    setTest(true);
+    return;
+  };
+
+  const db_data = { id: _id, status: newStatus };
+
+  fetch("http://localhost:5000/classes", {
+    method: "PATCH",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(db_data),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.insertedID) {
+        // console.log(data);
+      }
+    });
+
+  useEffect(() => {
+    if (status === "pending") {
+      setUnSelectable(false);
+      return;
+    } else if (status === "approved" || "denied") {
+      setUnSelectable(true);
+      return;
+    }
+  }, [status, db_data.status]);
+
+  return (
+    <div className="row align-items-center border p-2 m-2 rounded-4">
+      <div className="col-4">
+        <img src={image} alt="class image" className="img-fluid rounded-4" />
+      </div>
+      <div className="col-4">
+        <div className="fs-4 fw-light">{name}</div>
+        <div>Instructor : {instructor_name}</div>
+        <div>{instructor_email}</div>
+        <div>Price : {price}</div>
+        <div>Available Seats : {seats}</div>
+      </div>
+      <div className="col-2">{status}</div>
+      <div className="col-2">
+        <button
+          onClick={onApprove}
+          disabled={unSelectable ? true : false}
+          className="btn bg-dark text-white rounded-3 w-100 my-1"
+        >
+          Approve
+        </button>
+        <button
+          onClick={onDeny}
+          disabled={unSelectable ? true : false}
+          className="btn bg-dark text-white rounded-3 w-100 my-1"
+        >
+          Deny
+        </button>
+        <button
+          disabled={unSelectable ? true : false}
+          className="btn bg-dark text-white rounded-3 w-100 my-1"
+        >
+          Feedback
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default AdminSingleClass;
